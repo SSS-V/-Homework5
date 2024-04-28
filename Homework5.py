65e01077b4e075c81ed9279e74e2821eb8fa0ea8
SSS-V <167866942+SSS-V@users.noreply.github.com>
2024-04-28 17:27:12 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,14 @@
+#Список
+my_list= ["Apple", "Banana", "Pineapple", "kiwi", "lime"]
+print(my_list)
+print(my_list[0]), print(my_list[-1]) #Первое и последнее значение в списке.
+print(my_list[2:5]) #значения с 3-5
+my_list[2]= "Lemon"
+print(my_list)
+#Словарь
+my_dict = {"Work": "Работа", "Relax": "Отдых", "Kiwi": "Киви"}
+print(my_dict)
+print(my_dict["Work"])
+my_dict["Relax"] = "Кайфовать" # Изменение значения.
+my_dict["Apple"] = "Яблоко" # Добавил новое
+print(my_dict)
